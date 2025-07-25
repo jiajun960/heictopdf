de53b65d777ae9dc525db929f476f540e01836d8
jiajun960 <1021046253@qq.com>
2025-07-25 11:35:21 +0800

    添加GA代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VVGDWN5YDN"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VVGDWN5YDN');
+        ` }} />
+      </head>
       <body>{children}</body>
     </html>
   )
