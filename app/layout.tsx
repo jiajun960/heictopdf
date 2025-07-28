aa0cd9978a50abf19a44378be1cddf8ccb044e59
jiajun960 <1021046253@qq.com>
2025-07-28 10:43:31 +0800

    添加 Clarity 代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,14 @@ export default function RootLayout({
           gtag('js', new Date());
           gtag('config', 'G-VVGDWN5YDN');
         ` }} />
+        {/* Microsoft Clarity */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "slraxjos5c");
+        ` }} />
       </head>
       <body>{children}</body>
     </html>
